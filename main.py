24ea1005b0c4117be3b7f59ee97bb132e8514491
LCASTELLANOS <jcastel2@eafit.edu.co>
2021-11-18 22:17:26 -0500

    Create main.py
    Calcula el valor de GSI, D y mi en base a la ecuación potencial del macizo rocoso.

---
+++ main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import streamlit as st
+
+
+A = st.number_input('Valor de "A": ',value=0.16113, format="%.10f")
+B = st.number_input('Valor de "B"',value=0.71346, format="%.10f")
+stn = st.number_input('Valor del esfuerzo de tensión "𝜎𝑡"',value=-2.53E-04, format="%.10f")
+sci = st.number_input('Valor de resistencia a la compresión inconfinada de la roca intacta "𝜎ci [kPa]"',value=99600.0, format="%.10f")
+
+
+def evaluate(A, B, sci, stn):
+    
+    n_dots = 10000
+    n = np.linspace(2, 3, n_dots)
+    sn = n / sci
+
+    tn = A * (sn - stn) ** B
+    t = tn * sci
+    
+    beta = np.arctan(A * B * (sn - stn) ** (B - 1))
+    sprima = ((2 * np.sin(beta) * (1 + np.sin(beta))) / (np.cos(beta)) ** 2) + 1
+    s1 = n + t * sprima ** 0.5
+    s3 = n - t / sprima ** 0.5
+
+    GSI = np.linspace(1, 100, n_dots)
+    
+    result = []
+    for i, gsi in enumerate(GSI):        
+        a = 0.5 + 1 / 6 * (np.exp(-gsi / 15) - np.exp(-20 / 3))
+        mb = (sprima[0] - 1) / (a * ((s1[0] - s3[0]) / sci ) ** ((a - 1) / a))
+        s = ((s1[0] - s3[0]) / sci ) ** (1 / a) - mb * s3[0] / sci 
+        if 0 < s:
+            D = ((100 - gsi) / (3 * np.log(s))) + 3
+            if 0 < D and D <= 1:
+                mi = mb / np.exp((gsi - 100) / (28 - 14 * D))
+                s1_teo = s3 + sci * (mb * s3 / sci + s) ** a
+                if 0 < mi and mi <= 100:
+                    result.append(
+                        {
+                            "D": D,
+                            "mi": mi,
+                            "GSI": gsi,
+                            "s1": s1_teo.tolist(),
+                            "res": np.mean(abs(s1_teo - s1)),
+                            }
+                        )
+
+    min_res = min(result, key=lambda x: x["res"])
+    return min_res
+
+
+result = evaluate(A, B, sci, stn)
+
+st.title("Resultados")
+st.write(f'GSI: {result["GSI"]:.0f}')
+st.write(f'D: {result["D"]:.2f}')
+st.write(f'MI: {result["mi"]:.2f}')
+st.write(f'error: {result["res"]:.2f}')
